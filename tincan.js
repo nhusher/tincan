54a68a94edd6891bfb0149da4b96fc8c7736c675
Nick Husher <nhusher@gmail.com>
2014-06-11 19:37:18 -0700

    Removed nonsensical JS file

--- tincan.js
+++
@@ -1,4 +0,0 @@
-goog.addDependency("base.js", ['goog'], []);
-goog.addDependency("../cljs/core.js", ['cljs.core'], ['goog.string', 'goog.array', 'goog.object', 'goog.string.StringBuffer']);
-goog.addDependency("../tincan/core.js", ['tincan.core'], ['cljs.core']);
-goog.addDependency("../tincan/demo.js", ['tincan.demo'], ['cljs.core', 'tincan.core']);
